e6c3bb73224d31ccc4fe9403b473dc60ffea6c74
Sukkwon On <skwon2345@Sukkwons-MacBook-Pro.local>
2017-09-30 14:17:24 -0700

    add line graph

--- public/js/frothy.js
+++ public/js/frothy.js
@@ -46,6 +46,81 @@ d3.json("js/graphFile.json", function(json) {
         .attr("y2", function(d) { return d.target.y; });
 
     node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
   });
 });
 // end node connected graph
+
+//----------------------------------------------------------------------
+
+
+var svg_width = 500,
+   svg_height = 250,
+   data = [50, 60, 70, 80, 90, 100, 9, 7, 5, 3, 8, 90, 28, 35],
+   margin = {top:30, right: 20, bottom:40, left: 50},
+   chart_width = svg_width - margin.left - margin.right,
+   chart_height = svg_height - margin.top - margin.bottom
+
+var transfer_x = d3.scale.linear().domain([0, data.length]).range([0, chart_width]),
+   transfer_y = d3.scale.linear().domain([0, d3.max(data)]).range([chart_height, 0])
+
+var vis = d3.select("#recovery")
+.append("svg:svg")
+.attr("width", svg_width)
+.attr("height", svg_height)
+
+var draw_line = d3.svg.line()
+.x(function(d,i) { return transfer_x(i); })
+.y(function(d) { return transfer_y(d); })
+.interpolate("cardinal")
+
+var g = vis.append("svg:g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+g.append("svg:path")
+.attr("class", "area")
+.attr("d", draw_line(data));
+
+var x_axis = d3.svg.axis().scale(transfer_x).orient("bottom").ticks(10)
+var y_axis = d3.svg.axis().scale(transfer_y).orient("left").ticks(18)
+
+g.append("svg:g")
+.attr("class", "x axis")
+.call(x_axis)
+.attr("transform", "translate(0," + chart_height + ")")
+.append("text")
+.attr("x", 410)
+.attr("y", 37)
+.text("Time");
+
+g.append("svg:g")
+.attr("class", "y axis")
+.call(y_axis)
+.append("text")
+.attr("y", -30)
+.style("text-anchor", "end")
+.attr("transform", "rotate(-90)")
+.text("Recovery");
+
+var assays = [];
+
+// $(document).ready(function() {
+//   $.get('data.csv', function(data){ 
+    
+//     var lines = data.split('\n');
+//     var properties = ["Timestamp", "MF_Pb", "MF_Zn"]
+    
+//     for (var i = 0; i < lines.length; i++){
+//       var assay = lines[i].split(',');
+//       var tmp = {}
+//       for (var j = 0; j < assay.length; j++){
+//          tmp[properties[j]] = assay[j]
+//       }
+//       assays.push(tmp);
+//     }
+    
+//   });
+//   console.log.(assays);
+//   console.log(assays[0]);
+//   console.log(assays[0], ["Timestamp"]);
+// });
