d87441fe7106e0e9c76494c922ce3b7cd64a5fda
John L. Cao <john_caohaha@hotmail.com>
2017-09-30 16:53:40 -0700

    time sort data and add basic bar chart for operator recommendation

--- public/js/frothy.js
+++ public/js/frothy.js
@@ -1,55 +1,17 @@
-// node connected graph for recommendations to the operator
-var width = 960,
-    height = 500
+// operator recommendations graph
+var data = [
+    ["Air",0.012], ["Feed Grade",-0.025], ["Density",0.008], ["Frother",0.023], ["Activator",-0.009], ["Collector", 0.005]
+];
 
-var svg = d3.select("#operator").append("svg")
-    .attr("width", width)
-    .attr("height", height);
 
-var force = d3.layout.force()
-    .gravity(.05)
-    .distance(100)
-    .charge(-100)
-    .size([width, height]);
-
-d3.json("js/graphFile.json", function(json) {
-  force
-      .nodes(json.nodes)
-      .links(json.links)
-      .start();
-
-  var link = svg.selectAll(".link")
-      .data(json.links)
-    .enter().append("line")
-      .attr("class", "link")
-    .style("stroke-width", function(d) { return Math.sqrt(d.weight).toString(); })
-    .style("stroke", "#bbb");
-
-  var node = svg.selectAll(".node")
-      .data(json.nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .call(force.drag);
-
-  node.append("circle")
-      .attr("r","5");
-
-  node.append("text")
-      .attr("dx", 12)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name });
-
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-
-  });
-});
-// end node connected graph
+d3.select("#operator")
+  .datum(data)
+    .call(columnChart()
+      .width(960)
+      .height(500)
+      .x(function(d, i) { return d[0]; })
+      .y(function(d, i) { return d[1]; }));
+// end operator recommendations graph
 
 //----------------------------------------------------------------------
 
