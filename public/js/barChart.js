bb708e8fdaa6a4189a9b73a518dc7d46d61df456
John L. Cao <john_caohaha@hotmail.com>
2017-10-01 14:55:37 -0700

    add more data to chart

--- public/js/barChart.js
+++ public/js/barChart.js
@@ -252,7 +252,7 @@ var fakeAjax = function(iter, graph_num){
               "ZRCC Pb": 1.8032693863,
               "ZRCC Zn": 46.109272003,
               "ZRCC Fe": 78.2968492508,
-              "value": 0.3892795,
+              "value": 0,
               "qualityName_id": 1
             };
             break;
@@ -280,6 +280,419 @@ var fakeAjax = function(iter, graph_num){
           break;
       }
   }
+else if (iter===3) {
+    switch(graph_num) {
+      case 0:
+            return {
+              "MF Pb": 1.2458934784,
+              "MF Zn": 13.497014999,
+              "MF Fe": 62.4422984123,
+              "PbFCPb": 15.410709381,
+              "PbFC ZN": 61.606258392,
+              "PbFC Fe": 4.1280002594,
+              "ZRF Pb": 67.0963323116,
+              "ZRF Zn": 21.857028961,
+              "ZRF Fe": 95.1788711548,
+              "ZRT A Pb": 15.9203318357,
+              "ZRT A Zn": 1.9989464879,
+              "ZRT A Fe": 2.1686306,
+              "ZRT B Pb": 37.900001526,
+              "ZRT B Zn": 30.900001526,
+              "ZRT B Fe": 69.900001526,
+              "ZRT Comb Pb": 39.900001526,
+              "ZRT Comb Zn": 79.900001526,
+              "ZRT Comb Fe": 69.900001526,
+              "Z1RC Pb": 2.2828223705,
+              "Z1RC Zn": 33.465496063,
+              "Z1RC Fe": 4.6342253685,
+              "Z2RC Pb": 79.0510892868,
+              "Z2RC Zn": 6.221212387,
+              "Z2RC Fe": 64.158090591,
+              "ZRCC Pb": 3.8032693863,
+              "ZRCC Zn": 36.109272003,
+              "ZRCC Fe": 98.2968492508,
+              "value": 1,
+              "qualityName_id": 1
+            };
+            break;
+      case 1:
+          return {
+            "SAG1": 138.51496778/1.8,
+            "SAG2": 155.15563684/1.8,
+            "SAG3": 50.50767439/1.8,
+            "Pb Rghr Tails Flow": 3698.995226/40,
+            "Zn Rougher 2 Con Flow": 1913.59431435/40,
+            "Bank A Airflow": 1517.0152574/40,
+            "Bank B Airflow": 149.8928814/40
+          };
+          break;
+      case 2:
+          return{
+            "Z1RC 01 Output": 80,
+            "Z1RC 02 Output": 66,
+            "Z2RC 01 Output": 56,
+            "Z2RC 02 Output": 12
+          }
+          break;
+      default: 
+          return null;
+          break;
+      }
+    }
+  else if (iter===4) {
+    switch(graph_num) {
+      case 0:
+            return {
+              "MF Pb": 55.2458934784,
+              "MF Zn": 33.497014999,
+              "MF Fe": 47.4422984123,
+              "PbFCPb": 15.410709381,
+              "PbFC ZN": 61.606258392,
+              "PbFC Fe": 17.1280002594,
+              "ZRF Pb": 67.0963323116,
+              "ZRF Zn": 51.857028961,
+              "ZRF Fe": 25.1788711548,
+              "ZRT A Pb": 72.9203318357,
+              "ZRT A Zn": 12.9989464879,
+              "ZRT A Fe": 54.1686306,
+              "ZRT B Pb": 7.900001526,
+              "ZRT B Zn": 70.900001526,
+              "ZRT B Fe": 19.900001526,
+              "ZRT Comb Pb": 29.900001526,
+              "ZRT Comb Zn": 69.900001526,
+              "ZRT Comb Fe": 59.900001526,
+              "Z1RC Pb": 4.2828223705,
+              "Z1RC Zn": 63.465496063,
+              "Z1RC Fe": 1.6342253685,
+              "Z2RC Pb": 89.0510892868,
+              "Z2RC Zn": 2.221212387,
+              "Z2RC Fe": 54.158090591,
+              "ZRCC Pb": 1.8032693863,
+              "ZRCC Zn": 46.109272003,
+              "ZRCC Fe": 78.2968492508,
+              "value": 1,
+              "qualityName_id": 1
+            };
+            break;
+      case 1:
+          return {
+            "SAG1": 18.51496778/1.8,
+            "SAG2": 155.15563684/1.8,
+            "SAG3": 167.50767439/1.8,
+            "Pb Rghr Tails Flow": 3678.995226/40,
+            "Zn Rougher 2 Con Flow": 1613.59431435/40,
+            "Bank A Airflow": 417.0152574/40,
+            "Bank B Airflow": 2549.8928814/40
+          };
+          break;
+      case 2:
+          return{
+            "Z1RC 01 Output": 50,
+            "Z1RC 02 Output": 76,
+            "Z2RC 01 Output": 67,
+            "Z2RC 02 Output": 22
+          }
+          break;
+      default: 
+          return null;
+          break;
+      }
+    }
+  else if (iter===5) {
+    switch(graph_num) {
+      case 0:
+            return {
+              "MF Pb": 15.2458934784,
+              "MF Zn": 3.497014999,
+              "MF Fe": 67.4422984123,
+              "PbFCPb": 35.410709381,
+              "PbFC ZN": 51.606258392,
+              "PbFC Fe": 57.1280002594,
+              "ZRF Pb": 67.0963323116,
+              "ZRF Zn": 11.857028961,
+              "ZRF Fe": 55.1788711548,
+              "ZRT A Pb": 12.9203318357,
+              "ZRT A Zn": 7.9989464879,
+              "ZRT A Fe": 4.1686306,
+              "ZRT B Pb": 97.900001526,
+              "ZRT B Zn": 90.900001526,
+              "ZRT B Fe": 19.900001526,
+              "ZRT Comb Pb": 49.900001526,
+              "ZRT Comb Zn": 69.900001526,
+              "ZRT Comb Fe": 99.900001526,
+              "Z1RC Pb": 4.2828223705,
+              "Z1RC Zn": 33.465496063,
+              "Z1RC Fe": 6.6342253685,
+              "Z2RC Pb": 79.0510892868,
+              "Z2RC Zn": 2.221212387,
+              "Z2RC Fe": 74.158090591,
+              "ZRCC Pb": 1.8032693863,
+              "ZRCC Zn": 46.109272003,
+              "ZRCC Fe": 78.2968492508,
+              "value": 0,
+              "qualityName_id": 1
+            };
+            break;
+      case 1:
+          return {
+            "SAG1": 178.51496778/1.8,
+            "SAG2": 135.15563684/1.8,
+            "SAG3": 148.50767439/1.8,
+            "Pb Rghr Tails Flow": 2798.995226/40,
+            "Zn Rougher 2 Con Flow": 513.59431435/40,
+            "Bank A Airflow": 717.0152574/40,
+            "Bank B Airflow": 1249.8928814/40
+          };
+          break;
+      case 2:
+          return{
+            "Z1RC 01 Output": 70,
+            "Z1RC 02 Output": 87,
+            "Z2RC 01 Output": 89,
+            "Z2RC 02 Output": 29
+          }
+          break;
+      default: 
+          return null;
+          break;
+      }
+    }
+  else if (iter===6) {
+    switch(graph_num) {
+      case 0:
+            return {
+              "MF Pb": 15.2458934784,
+              "MF Zn": 3.497014999,
+              "MF Fe": 67.4422984123,
+              "PbFCPb": 35.410709381,
+              "PbFC ZN": 51.606258392,
+              "PbFC Fe": 7.1280002594,
+              "ZRF Pb": 17.0963323116,
+              "ZRF Zn": 34.857028961,
+              "ZRF Fe": 85.1788711548,
+              "ZRT A Pb": 12.9203318357,
+              "ZRT A Zn": 7.9989464879,
+              "ZRT A Fe": 4.1686306,
+              "ZRT B Pb": 97.900001526,
+              "ZRT B Zn": 40.900001526,
+              "ZRT B Fe": 69.900001526,
+              "ZRT Comb Pb": 49.900001526,
+              "ZRT Comb Zn": 69.900001526,
+              "ZRT Comb Fe": 99.900001526,
+              "Z1RC Pb": 4.2828223705,
+              "Z1RC Zn": 33.465496063,
+              "Z1RC Fe": 6.6342253685,
+              "Z2RC Pb": 69.0510892868,
+              "Z2RC Zn": 2.221212387,
+              "Z2RC Fe": 74.158090591,
+              "ZRCC Pb": 1.8032693863,
+              "ZRCC Zn": 46.109272003,
+              "ZRCC Fe": 78.2968492508,
+              "value": 1,
+              "qualityName_id": 1
+            };
+            break;
+      case 1:
+          return {
+            "SAG1": 68.51496778/1.8,
+            "SAG2": 15.15563684/1.8,
+            "SAG3": 118.50767439/1.8,
+            "Pb Rghr Tails Flow": 3598.995226/40,
+            "Zn Rougher 2 Con Flow": 993.59431435/40,
+            "Bank A Airflow": 1217.0152574/40,
+            "Bank B Airflow": 549.8928814/40
+          };
+          break;
+      case 2:
+          return{
+            "Z1RC 01 Output": 13,
+            "Z1RC 02 Output": 34,
+            "Z2RC 01 Output": 46,
+            "Z2RC 02 Output": 78
+          }
+          break;
+      default: 
+          return null;
+          break;
+      }
+    }
+  else if (iter===7) {
+    switch(graph_num) {
+      case 0:
+            return {
+              "MF Pb": 15.2458934784,
+              "MF Zn": 3.497014999,
+              "MF Fe":57.4422984123,
+              "PbFCPb": 25.410709381,
+              "PbFC ZN": 51.606258392,
+              "PbFC Fe": 7.1280002594,
+              "ZRF Pb": 67.0963323116,
+              "ZRF Zn": 11.857028961,
+              "ZRF Fe": 75.1788711548,
+              "ZRT A Pb": 12.9203318357,
+              "ZRT A Zn": 7.9989464879,
+              "ZRT A Fe": 4.1686306,
+              "ZRT B Pb": 97.900001526,
+              "ZRT B Zn": 90.900001526,
+              "ZRT B Fe": 89.900001526,
+              "ZRT Comb Pb": 49.900001526,
+              "ZRT Comb Zn": 69.900001526,
+              "ZRT Comb Fe": 99.900001526,
+              "Z1RC Pb": 4.2828223705,
+              "Z1RC Zn": 45.465496063,
+              "Z1RC Fe": 6.6342253685,
+              "Z2RC Pb": 39.0510892868,
+              "Z2RC Zn": 2.221212387,
+              "Z2RC Fe": 74.158090591,
+              "ZRCC Pb": 1.8032693863,
+              "ZRCC Zn": 86.109272003,
+              "ZRCC Fe": 78.2968492508,
+              "value": 0,
+              "qualityName_id": 1
+            };
+            break;
+      case 1:
+          return {
+            "SAG1": 78.51496778/1.8,
+            "SAG2": 125.15563684/1.8,
+            "SAG3": 56.50767439/1.8,
+            "Pb Rghr Tails Flow": 2798.995226/40,
+            "Zn Rougher 2 Con Flow": 913.59431435/40,
+            "Bank A Airflow": 117.0152574/40,
+            "Bank B Airflow": 1549.8928814/40
+          };
+          break;
+      case 2:
+          return{
+            "Z1RC 01 Output": 50,
+            "Z1RC 02 Output": 76,
+            "Z2RC 01 Output": 36,
+            "Z2RC 02 Output": 92
+          }
+          break;
+      default: 
+          return null;
+          break;
+      }
+    }
+  else if (iter===8) {
+    switch(graph_num) {
+      case 0:
+            return {
+              "MF Pb": 15.2458934784,
+              "MF Zn": 3.497014999,
+              "MF Fe": 67.4422984123,
+              "PbFCPb": 35.410709381,
+              "PbFC ZN": 51.606258392,
+              "PbFC Fe": 7.1280002594,
+              "ZRF Pb": 27.0963323116,
+              "ZRF Zn": 41.857028961,
+              "ZRF Fe": 65.1788711548,
+              "ZRT A Pb": 12.9203318357,
+              "ZRT A Zn": 7.9989464879,
+              "ZRT A Fe": 4.1686306,
+              "ZRT B Pb": 57.900001526,
+              "ZRT B Zn": 90.900001526,
+              "ZRT B Fe": 49.900001526,
+              "ZRT Comb Pb": 49.900001526,
+              "ZRT Comb Zn": 69.900001526,
+              "ZRT Comb Fe": 99.900001526,
+              "Z1RC Pb": 4.2828223705,
+              "Z1RC Zn": 36.465496063,
+              "Z1RC Fe": 6.6342253685,
+              "Z2RC Pb": 39.0510892868,
+              "Z2RC Zn": 2.221212387,
+              "Z2RC Fe": 74.158090591,
+              "ZRCC Pb": 1.8032693863,
+              "ZRCC Zn": 46.109272003,
+              "ZRCC Fe": 78.2968492508,
+              "value": 1,
+              "qualityName_id": 1
+            };
+            break;
+      case 1:
+          return {
+            "SAG1": 123.51496778/1.8,
+            "SAG2": 34.15563684/1.8,
+            "SAG3": 75.50767439/1.8,
+            "Pb Rghr Tails Flow": 3148.995226/40,
+            "Zn Rougher 2 Con Flow": 113.59431435/40,
+            "Bank A Airflow": 1547.0152574/40,
+            "Bank B Airflow": 119.8928814/40
+          };
+          break;
+      case 2:
+          return{
+            "Z1RC 01 Output": 10,
+            "Z1RC 02 Output": 56,
+            "Z2RC 01 Output": 76,
+            "Z2RC 02 Output": 32
+          }
+          break;
+      default: 
+          return null;
+          break;
+      }
+    }
+  else if (iter===9) {
+    switch(graph_num) {
+      case 0:
+            return {
+              "MF Pb": 15.2458934784,
+              "MF Zn": 3.497014999,
+              "MF Fe": 12.4422984123,
+              "PbFCPb": 35.410709381,
+              "PbFC ZN": 51.606258392,
+              "PbFC Fe": 7.1280002594,
+              "ZRF Pb": 37.0963323116,
+              "ZRF Zn": 65.857028961,
+              "ZRF Fe": 35.1788711548,
+              "ZRT A Pb": 12.9203318357,
+              "ZRT A Zn": 7.9989464879,
+              "ZRT A Fe": 4.1686306,
+              "ZRT B Pb": 97.900001526,
+              "ZRT B Zn": 76.900001526,
+              "ZRT B Fe": 34.900001526,
+              "ZRT Comb Pb": 49.900001526,
+              "ZRT Comb Zn": 69.900001526,
+              "ZRT Comb Fe": 39.900001526,
+              "Z1RC Pb": 4.2828223705,
+              "Z1RC Zn": 57.465496063,
+              "Z1RC Fe": 6.6342253685,
+              "Z2RC Pb": 89.0510892868,
+              "Z2RC Zn": 2.221212387,
+              "Z2RC Fe": 74.158090591,
+              "ZRCC Pb": 1.8032693863,
+              "ZRCC Zn": 46.109272003,
+              "ZRCC Fe": 46.2968492508,
+              "value": 0,
+              "qualityName_id": 1
+            };
+            break;
+      case 1:
+          return {
+            "SAG1": 78.51496778/1.8,
+            "SAG2": 165.15563684/1.8,
+            "SAG3": 148.50767439/1.8,
+            "Pb Rghr Tails Flow": 3198.995226/40,
+            "Zn Rougher 2 Con Flow": 913.59431435/40,
+            "Bank A Airflow": 1417.0152574/40,
+            "Bank B Airflow": 1549.8928814/40
+          };
+          break;
+      case 2:
+          return{
+            "Z1RC 01 Output": 12,
+            "Z1RC 02 Output": 43,
+            "Z2RC 01 Output": 46,
+            "Z2RC 02 Output": 72
+          }
+          break;
+      default: 
+          return null;
+          break;
+      }
+    }
 }
 
 //Pulls data
@@ -291,9 +704,8 @@ var pullData = function(settings,callback, graph_num){
     // if (err) return console.warn(err);
     // console.log(data_index);
     var newData = fakeAjax(data_index[graph_num], graph_num);
-    if (data_index[graph_num] === 1){
-      data_index[graph_num] = 2;  
-    }else {
+    data_index[graph_num]++;
+    if (data_index[graph_num] === 10) {
       data_index[graph_num] = 1;
     }
     
